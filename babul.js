03f5338297f72c6f7891a3c65d9e8c559517880c
srijan rout <152413654+babul-1234@users.noreply.github.com>
2024-07-27 08:27:57 +0530

    Add files via upload

---
+++ babul.js
@@ -0,0 +1,39 @@
+document.getElementById('srijan').addEventListener('click', function() {
+    const button = document.getElementById('srijan');
+    if (button) {
+        button.remove();
+    } else {
+        console.log("not remove");
+    }
+
+});
+document.getElementById('rout').addEventListener('click', function() {
+    const button = document.getElementById('rout');
+    if (button) {
+        button.remove();
+    } else {
+        console.log("not  a button");
+    }
+
+
+});
+document.getElementById('babul').addEventListener('click', function() {
+    const button = document.getElementById('babul');
+    if (button) {
+        button.remove();
+    } else {
+        console.log("not  a button");
+    }
+
+
+});
+document.getElementById('babuni').addEventListener('click', function() {
+    const button = document.getElementById('babuni');
+    if (button) {
+        button.remove();
+    } else {
+        console.log("not  a button");
+    }
+
+
+});
