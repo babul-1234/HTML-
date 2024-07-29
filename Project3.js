fe9ffa112450eff2ca94242bf54d82851e7b3707
srijan rout <152413654+babul-1234@users.noreply.github.com>
2024-07-29 20:10:40 +0530

    Add files via upload

---
+++ Project3.js
@@ -0,0 +1,11 @@
+const select = document.querySelector('#babul');
+const button = document.querySelector('#rout');
+button.addEventListener('click', function() {
+    if (select) {
+        select.innerHTML = "MY NAME IS SRIJAN ROUT AND MY HOBBY IS to PLAYING CRICKET";
+    } else {
+        select.remove
+    }
+
+
+});
